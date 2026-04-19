81c23ef384f655fd6c34eca3e93c20f2c13466bb
siddhartha-pant <iamsiddhartha23@gmail.com>
2026-04-19 14:23:08 +0530

    navbar changed

--- src/features/auth/components/Navbar.jsx
+++ src/features/auth/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { useAuth } from "../hooks/useAuth";
-import "./Navbar.css";
+import "./navbar.css";
 
 const Navbar = () => {
   const { user, handleLogout } = useAuth();
